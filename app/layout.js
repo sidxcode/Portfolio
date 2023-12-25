ad3d699a3eac0f74c1990c0ef4a5a5cf65dbb293
Siddharth Borman <siddharth02b@gmail.com>
2023-12-25 13:40:36 +0530

    nextui and navbr added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import * as React from 'react'
+import Providers from './providers'
+import Navbar from './components/Navbr'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar></Navbar>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
